f15ed0215bd759e3ad396a3aaa26adeb7d04dddc
Luis Felipe Evilla Rodriguez <luisevilla588@gmail.com>
2025-11-06 14:02:40 -0500

    Update server port in main.ts from 3000 to 3002 for application listening

--- src/main.ts
+++ src/main.ts
@@ -92,6 +92,6 @@ async function bootstrap() {
       defaultModelExpandDepth: 2,
     },
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
